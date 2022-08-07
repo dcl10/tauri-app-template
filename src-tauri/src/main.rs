157d71657d2ecfb70ab17e308e0619dccf69e819
Daniel Lea <dan.carr.lea@gmail.com>
2022-08-07 18:38:45 +0100

    backend increase and decrease functions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,17 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![increase, decrease])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
+
+#[tauri::command]
+fn increase(num: i32) -> i32 {
+  num + 1
+} 
+
+#[tauri::command]
+fn decrease(num: i32) -> i32 {
+  num - 1
+} 
