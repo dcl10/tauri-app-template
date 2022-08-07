ad3791202360ff7a309848e3f90738ead2e95241
Daniel Lea <dan.carr.lea@gmail.com>
2022-08-07 18:49:47 +0100

    rust docs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,11 +10,33 @@ fn main() {
     .expect("error while running tauri application");
 }
 
+/// A tauri command to return the supplied number `num` + 1.
+/// 
+/// # Arguments:
+/// * num: the number to be increased.
+/// 
+/// # Example
+/// `example.js`
+/// ```javascript
+/// invoke('increase', {num: 1})
+/// // 2
+/// ```
 #[tauri::command]
 fn increase(num: i32) -> i32 {
   num + 1
 } 
 
+/// A tauri command to return the supplied number `num` - 1.
+/// 
+/// # Arguments:
+/// * num: the number to be decreased.
+/// 
+/// # Example
+/// `example.js`
+/// ```javascript
+/// invoke('decrease', {num: 1})
+/// // 0
+/// ```
 #[tauri::command]
 fn decrease(num: i32) -> i32 {
   num - 1
